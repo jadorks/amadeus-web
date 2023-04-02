559bcbe3c3eaeca86eb523b70deb6b5112cf46be
jadorks <adorkorj@gmail.com>
2023-04-02 12:45:19 +0000

    Fix title

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Amodeus</title>
+        <title>Amadeus</title>
 
         <link
           rel="apple-touch-icon"
